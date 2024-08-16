35ee49795fd9e1737838c63914f25e811b8018cf
EmreCanTERKAN <emrecanterkan06@gmail.com>
2024-08-16 10:56:50 +0300

    Add project files.

---
+++ Week5-Practice5/Program.cs
@@ -0,0 +1,21 @@
+﻿bool kosul = false;
+
+while (!kosul)
+{
+    try
+    {
+        Console.WriteLine("Bir sayı gir");
+        int sayi = int.Parse(Console.ReadLine());
+        Console.WriteLine("Sayının Karesi : " + sayi * sayi);
+        kosul = true;
+
+
+    }
+    catch (Exception)
+    {
+
+        Console.WriteLine("Hata...Geçersiz Sayı Girdin. Lütfen Bir sayı giriniz");
+    }
+
+}
+
